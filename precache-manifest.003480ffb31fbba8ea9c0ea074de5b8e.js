ffc30afd7035418d2b6c4295347647f3fe31f937
Saravanan Selvaraju <selvaras@optum.com>
2020-04-08 21:53:48 +0530

    Updates

---
+++ precache-manifest.003480ffb31fbba8ea9c0ea074de5b8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95666d87cef19987a35632ee37d665fe",
+    "url": "/react-bartender/index.html"
+  },
+  {
+    "revision": "5fe5c3a3ff25d49d29f7",
+    "url": "/react-bartender/static/css/main.17654223.chunk.css"
+  },
+  {
+    "revision": "fd1f2d4bc0a3465ab1d7",
+    "url": "/react-bartender/static/js/2.3583d209.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-bartender/static/js/2.3583d209.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5fe5c3a3ff25d49d29f7",
+    "url": "/react-bartender/static/js/main.d875d3d6.chunk.js"
+  },
+  {
+    "revision": "77d89e756c4df4d49631",
+    "url": "/react-bartender/static/js/runtime-main.0e028322.js"
+  }
+]);
