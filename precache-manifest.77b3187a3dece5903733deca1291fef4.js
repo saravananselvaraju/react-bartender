7c991e1a378cbe9d25c64d8dad39c7322ea1d018
Saravanan Selvaraju <selvaras@optum.com>
2020-04-23 19:21:52 +0530

    Updates

--- precache-manifest.77b3187a3dece5903733deca1291fef4.js
+++ precache-manifest.77b3187a3dece5903733deca1291fef4.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "588c6d46645bbff8234b68448625849e",
+    "revision": "7778f172224c72374a1201be8169a2c9",
     "url": "/react-bartender/index.html"
   },
   {
-    "revision": "d12a60bf4d79acc7a922",
+    "revision": "9ae8badc82aad46974e0",
     "url": "/react-bartender/static/css/main.a0689d7d.chunk.css"
   },
   {
-    "revision": "51851e9e65946d8cedf2",
-    "url": "/react-bartender/static/js/2.0f87bbdd.chunk.js"
+    "revision": "1b1276f9bb5064d76859",
+    "url": "/react-bartender/static/js/2.2ddcc46d.chunk.js"
   },
   {
     "revision": "e928fe768baa9832b5bc57eae021f30c",
-    "url": "/react-bartender/static/js/2.0f87bbdd.chunk.js.LICENSE"
+    "url": "/react-bartender/static/js/2.2ddcc46d.chunk.js.LICENSE"
   },
   {
-    "revision": "d12a60bf4d79acc7a922",
-    "url": "/react-bartender/static/js/main.ebea9dfc.chunk.js"
+    "revision": "9ae8badc82aad46974e0",
+    "url": "/react-bartender/static/js/main.672dcf6b.chunk.js"
   },
   {
     "revision": "77d89e756c4df4d49631",
