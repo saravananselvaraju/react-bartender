2f19e37e9ec43dc8ca94c2aa140487d81d77b0d7
Saravanan Selvaraju <saravanan036@outlook.com>
2025-12-06 10:25:51 +0530

    cosmetic changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
     return (
         <div className="Footer">
-            <p> Github <a href="https://github.com/saravanan-selvaraju/react-bartender">react-bartender</a></p>
+            <p> Github <a href="https://github.com/saravananselvaraju/react-bartender">react-bartender</a></p>
         </div>
     )
 }
